23c5a96e7f19c40eaf1b1602c0271f5a977424a5
Mariam Jaludi <mariam.jaludi@gmail.com>
2018-11-10 21:17:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,13 +15,14 @@ function logWhisper(string){
 }
 
 function sayHiToGrandma(string){
-  if (string === string.toLowerCase){
+
+  if (string === string.toLowerCase()){
     return "I can't hear you!"
   }
-  else {
+  else if(string === string.toUpperCase()){
     return "YES INDEED!"
   }
-  if (string === "I love you, Grandma."){
+  if(string === "I love you, Grandma."){
     return "I love you, too."
   }
 }
